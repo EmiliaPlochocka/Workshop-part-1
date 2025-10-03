04df5ae7a8d7494f9c8bbd0e056a01aa63cdc459
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-03 16:44:48 +0200

    Updated 'read json' script according to blog
    Updated script to loop through location list and create a report text file

--- read-network-devices.py
+++ read-network-devices.py
@@ -1,7 +1,20 @@
 # import json library
 import json
 
-# read json from network-devices.json into variable devices
-devices = json.load(open("network-devices.json","r",encoding = "utf-8"))
+# read json from network-devices.json into variable data
+data = json.load(open("network-devices.json","r",encoding = "utf-8"))
 
-print(devices)
+# created an empty variable that will hold report text
+report = ""
+
+# loop through location list
+for location in data["locations"]:
+    # include site name in report
+    report += "\n" + location["site"] + "\n"
+    # include device host names from each location in report
+    for device in location["devices"]:
+        report += " " + device["hostname"] + "\n"
+
+# write data into a text file
+with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write(report)

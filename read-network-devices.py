f7bfb2e10a0d9fee77ea0916df6c367570fa5e33
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-04 17:26:40 +0200

    Updated comments

--- read-network-devices.py
+++ read-network-devices.py
@@ -7,9 +7,9 @@ data = json.load(open("network-devices.json","r",encoding = "utf-8"))
 # create an empty variable that will hold report text
 report = ""
 
+# include company name and date of last update in report
 for name in data["company"]:
     report += name
-
 for date in data["last_updated"]:
     report += date
 
@@ -21,14 +21,6 @@ for location in data["locations"]:
     # include device host names from each location in report
     for device in location["devices"]:
         report += " " + device["hostname"] + "\n"
-
-# for company in data["company"]:
-#     report += "\n" + company[""] + "\n"
-
-
-# for last_update in data["last_update"]:
-#     report += " " + last_update + " "
-
 
 # write data into a text file
 with open('report.txt', 'w', encoding='utf-8') as f:

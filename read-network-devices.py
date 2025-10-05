dc7b5f7dec1b7dde279c3dd66210857277d4154b
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-05 18:15:04 +0200

    Cleared formatting

--- read-network-devices.py
+++ read-network-devices.py
@@ -7,32 +7,54 @@ data = json.load(open("network-devices.json","r",encoding = "utf-8"))
 # create an empty variable that will hold report text
 report = ""
 
-# include company name and date of last update in report
-for name in data["company"]:
-    report += name
-for date in data["last_updated"]:
-    report += date
-
-# loop through location list
+# loop through devices with uptime of less than 30 days
+report += "\n" + "Devices with less than 30 days of uptime:\n"
 for location in data["locations"]:
     # include site name in report
     report += "\n" + location["site"] + "\n"
-    # include device host names from each location in report
+    # include devices in report
     for device in location["devices"]:
         if device["uptime_days"] <30:
             report += (" "
                      + device["hostname"].ljust(15,' ') + ' '
-                     + device["status"].ljust(15) + ' '
+                     + "Uptime days:".ljust(15) + ' '
                      + str(device["uptime_days"]).rjust(4) + "\n"
                      )
 
-# create an empty summary
-summary = ""
-summary += "Summary:\n"
-summary += "This is our basic report:\n"
+# loop through devices with offline status
+report += "\n" + "Devices with 'offline' or 'warning' status:\n"
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["status"] == "offline":
+            report += "\n" + location["site"] + "\n"
+            report += (" "
+                     + device["hostname"].ljust(15,' ') + ' '
+                     + (device["status"]).rjust(4) + "\n"
+                     )
+            
+# loop through devices with warning status
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["status"] == "warning":
+            report += "\n" + location["site"] + "\n"
+            report += (" "
+                     + device["hostname"].ljust(15,' ') + ' '
+                     + (device["status"]).rjust(4) + "\n"
+                     )
+
+# create an empty header
+header = ""
+header += "Networking report for:\n"
+# include company name and date of last update in header
+for name in data["company"]:
+    header += name
+header += "\n"
+for date in data["last_updated"]:
+    header += date
+header += "\n"
     
 # add summary before main report
-report = summary + report
+report = header + report
 
 
 # write data into a text file

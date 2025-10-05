a6171d0a5689ce8ded596f2d2cb3d7b7d29266b5
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-05 22:49:50 +0200

    Update read-network-devices.py

--- read-network-devices.py
+++ read-network-devices.py
@@ -7,6 +7,7 @@ data = json.load(open("network-devices.json","r",encoding = "utf-8"))
 # create an empty variable that will hold report text
 report = ""
 
+# ___HEADER___
 # create an empty header
 header = ""
 header += "Networking report for:\n"
@@ -17,10 +18,10 @@ header += "\n"
 for date in data["last_updated"]:
     header += date
 header += "\n"
-    
-# add summary before main report
+# add header before main report
 report = header + report
 
+#___LOW UPTIME___
 # loop through devices with uptime of less than 30 days
 report += "\n" + "Devices with less than 30 days of uptime:\n"
 for location in data["locations"]:
@@ -35,6 +36,7 @@ for location in data["locations"]:
                      + str(device["uptime_days"]).rjust(4) + "\n"
                      )
 
+#___STATUS___
 # loop through devices with offline or warning status
 report += "\n" + "Devices with 'offline' or 'warning' status:\n"
 for location in data["locations"]:
@@ -45,6 +47,12 @@ for location in data["locations"]:
                      + device["hostname"].ljust(15,' ') + ' '
                      + (device["status"]).rjust(4) + "\n"
                      )
+#___COUNT DEVICES___
+counts = {}
+for "router" in "type":
+    if "router" not in counts:
+        counts[item] = 0
+    counts[item] += 1
 
 # write data into a text file
 with open('report.txt', 'w', encoding='utf-8') as f:

87f81a7d660dfba846d284efd3ac97fd55864c8b
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-07 18:23:51 +0200

    added device counter

--- read-network-devices.py
+++ read-network-devices.py
@@ -48,12 +48,38 @@ for location in data["locations"]:
                      + (device["status"]).rjust(4) + "\n"
                      )
 #___COUNT DEVICES___
+# create new dictionary and add start values
 counts = {}
-for "router" in "type":
-    if "router" not in counts:
-        counts[item] = 0
-    counts[item] += 1
+counts = {'switch': 0, 'router': 0, 'access_point': 0, 'firewall': 0, 'load_balancer': 0}
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["type"] == "switch":
+            counts['switch'] += 1
+        if device["type"] == "router":
+            counts['router'] += 1
+        if device["type"] == "access_point":
+            counts['access_point'] += 1
+        if device["type"] == "firewall":
+            counts['firewall'] += 1
+        if device["type"] == "load_balancer":
+            counts['load_balancer'] += 1
 
-# write data into a text file
+# fetch value
+count_switch = counts["switch"]
+count_router = counts["router"]
+count_access_point = counts["access_point"]
+count_firewall = counts["firewall"]
+count_load_balancer = counts["load_balancer"]
+
+# add sum of devices to report
+report += "\n" + "List of all devices by type:\n"
+report += "\n" + "number of switches:" + " " + str(count_switch) + "\n"
+report += "number of routers:" + " " + str(count_router) + "\n"
+report += "number of switcaccess points:" + " " + str(count_access_point) + "\n"
+report += "number of firewalls:" + " " + str(count_firewall) + "\n"
+report += "number of load balancers:" + " " + str(count_load_balancer) + "\n"
+
+
+#___write data into a text file___
 with open('report.txt', 'w', encoding='utf-8') as f:
     f.write(report)
